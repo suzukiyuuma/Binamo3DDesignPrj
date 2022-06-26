0e1eff50546be30140adb31bdd928d559f31bff4
suzukiyuuma <yuumasuzuki0402@gmail.com>
2022-06-26 18:17:48 +0900

    seederによってusersテーブルにテストデータを作成に成功

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -4,10 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
